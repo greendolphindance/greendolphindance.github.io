c960e88413c0d56d276c062ca0678dd10b9b0ea9
greendolphindance <chumiao_chen@icloud.com>
2023-10-19 17:13:37 -0700

    Site updated: 2023-10-19 17:13:37

--- js/totop.js
+++ js/totop.js
@@ -1,25 +1,26 @@
-(function($) {
+ (function($) {
      // When to show the scroll link
      // higher number = scroll link appears further down the page
-     var upperLimit = 1000;
+     var upperLimit = 1000; // 您可以根据需要调整此值
      
      // Our scroll link element
      var scrollElem = $('#totop');
-    
+     
      // Scroll to top speed
      var scrollSpeed = 500;
-    
-     // Show and hide the scroll to top link based on scroll position
+     
+     // Hide the scroll to top link initially
      scrollElem.hide();
+     
      $(window).scroll(function () {
          var scrollTop = $(document).scrollTop();
-         if ( scrollTop > upperLimit ) {
-             $(scrollElem).stop().fadeTo(300, 1); // fade back in
-         }else{
-             $(scrollElem).stop().fadeTo(300, 0); // fade out
+         if (scrollTop > upperLimit) {
+             $(scrollElem).stop().fadeIn(300); // fade in
+         } else {
+             $(scrollElem).stop().fadeOut(300); // fade out and hide
          }
      });
-
+     
      // Scroll to top animation on click
      $(scrollElem).click(function(){
          $('html, body').animate({scrollTop:0}, scrollSpeed); return false;
