672a06a6d71d7004b2c7cfbe03068cebaf0a7bc9
greendolphindance <chumiao_chen@icloud.com>
2025-07-31 14:02:33 +0800

    Forcefully add lowercase chic theme for real

--- themes/Chic/scripts/hexo-yaml-helper.js
+++
@@ -1,7 +0,0 @@
-const yaml = require('js-yaml');
-const fs = require('fs');
-
-hexo.extend.helper.register('getYaml', function (path) {
-    const content = fs.readFileSync(path, 'utf8');
-    return yaml.load(content);
-});
